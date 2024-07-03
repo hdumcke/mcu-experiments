c9231a070c73680224f7ca081ac4f93e86702249
Horst Dumcke <hdumcke@marlysys.com>
2024-07-03 21:47:45 +0200

    fixed zephyr/button_esp32.sh

--- zephyr/button_esp32.sh
+++ zephyr/button_esp32.sh
@@ -9,8 +9,35 @@ cp -r ~/zephyrproject/zephyr/samples/basic/button button_esp32
 cd button_esp32
 source ~/zephyrproject/zephyr/zephyr-env.sh
 
+cat > app.overlay << EOF
+/ {
+
+    gpio_keys {
+        compatible = "gpio-keys";
+        user_button: button {
+            label = "User";
+            gpios = <&gpio0 0 GPIO_ACTIVE_LOW>;
+        };
+    };
+
+    aliases {
+        sw0 = &user_button;
+        led3 = &ext_led;
+    };
+    leds {
+        compatible = "gpio-leds";
+        ext_led: led_3 {
+            gpios = < &gpio0 0x11 0x0 >;
+            label = "Ext - LED";
+        };
+    };
+};
+EOF
+
+sed -i "s/led0/led3/" src/main.c
+
 west update
 west blobs fetch hal_espressif
 west build -b esp_wrover_kit
-#west flash
-#west espressif monitor
+west flash
+west espressif monitor
